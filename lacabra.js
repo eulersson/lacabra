395e7b232817d3e8b0568740c0c17cc4ce7eb3fe
Ramon Blanquer <blanquer.ramon@gmail.com>
2017-02-25 13:11:52 +0000

    Dragging works. Needs refactor.

--- lacabra.js
+++ lacabra.js
@@ -3,10 +3,12 @@ function Node(x, y, w, h) {
   this.y = y;
   this.w = w;
   this.h = h;
+  this.moving = false;
 }
 
-Node.prototype.update = function() {
-
+Node.prototype.move = function(dx, dy) {
+  this.x += dx;
+  this.y += dy;
 }
 
 Node.prototype.isHit = function(x, y) {
@@ -28,50 +30,72 @@ Node.prototype.draw = function(ctx) {
 }
 
 function Graph(id) {
-  this.id = id
-
+  this.id = id;
+  this.startX = 0;
+  this.startY = 0;
   this.mouseX = 0;
   this.mouseY = 0;
   this.mousePressed = false;
   this.dragging = false;
-
   this.nodes = [];
 }
 
 Graph.prototype.initialize = function() {
 
-  var self = this;
-
   this.canvas = document.getElementById(this.id);
-
-  this.canvas.addEventListener('mousemove', function(e) {
-    self.mouseX = e.offsetX;
-    self.mouseY = e.offsetY;
-  })
-
-  this.canvas.onmousedown = function() {
-    self.mousePressed = true;
-  }
-
-  this.canvas.onmouseup = function() {
-    self.mousePressed = false;
-  }
-
-  this.width = this.canvas.width;
+  this.width  = this.canvas.width;
   this.height = this.canvas.height;
-  this.ctx = this.canvas.getContext("2d");
+  this.ctx    = this.canvas.getContext("2d");
+
+  var mouseMoveCallback = function(e) {
+    this.mouseX = e.offsetX;
+    this.mouseY = e.offsetY;
+  }
+
+  var mouseDownCallback = function() {
+    this.mousePressed = true;
+  }
+
+  var mouseUpCallback = function() {
+    this.mousePressed = false;
+    this.dragging = false;
+  }
+
+  this.canvas.addEventListener('mousemove', mouseMoveCallback.bind(this));
+  this.canvas.onmousedown = mouseDownCallback.bind(this);
+  this.canvas.onmouseup   = mouseUpCallback.bind(this);
 }
 
 Graph.prototype.update = function() {
   if (this.mousePressed) {
-    var self = this;
-    console.log(this);
+    if (!this.dragging) {
+      this.startX = this.mouseX;
+      this.startY = this.mouseY;
+    }
+    this.dragging = true;
     this.nodes.forEach(function(node) {
-      if (node.isHit(self.mouseX, self.mouseY)) {
-        console.log(this);
-        console.log("HIT!!!");
-      }
-    })
+      if (node.isHit(this.mouseX, this.mouseY)) {
+        if (!node.moving) {
+          console.log("Start dragging node.");
+          node.moving = true;
+        } else {
+          console.log("Keep moving the node.");
+          console.log(this.mouseX - this.startX);
+          var dx = this.mouseX - this.startX;
+          var dy = this.mouseY - this.startY;
+          node.move(dx, dy);
+          this.startX = this.mouseX;
+          this.startY = this.mouseY;
+        }
+      } else {
+        console.log("Did not hit anything.");
+      } 
+    }, this);
+  } else {
+    console.log("Stop dragging everything");
+    this.nodes.forEach(function(node) {
+      node.moving = false;
+    });
   }
 }
 
@@ -91,8 +115,10 @@ Graph.prototype.addNode = function(node) {
 
 var graph = new Graph('graph');
 graph.initialize();
-var node = new Node(10, 10, 20, 20);
+var node = new Node(10, 10, 70, 70);
 graph.addNode(node);
+var node1 = new Node ( 100, 100, 30, 30);
+graph.addNode(node1);
 
 setInterval(function() {
   graph.update();
